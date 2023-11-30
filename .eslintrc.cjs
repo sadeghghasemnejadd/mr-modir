df37a51b394c0f5a6042636e08e4eeac0e130782
sadegh <gahsemnejad.sadegh@gmail.com>
2023-11-30 22:17:47 +0330

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,44 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'eslint:recommended',
+  ],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: [
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+      ],
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint'],
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [
+      1,
+      { extensions: ['.js', '.jsx', '.tsx', '.ts'] },
+    ],
+    semi: 0,
+    '@typescript-eslint/semi': 'off',
+    'no-unexpected-multiline': 'error',
+    'trailing-comma': 0,
+    '@typescript-eslint/comma-dangle': 0,
+  },
+};
