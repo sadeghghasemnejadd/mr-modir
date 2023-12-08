acb9d6d7393929fbd921d4dceaed082bfa5db375
sadegh <gahsemnejad.sadegh@gmail.com>
2023-12-08 17:32:13 +0330

    dd title dynamically

--- src/ui/layout/AppLayout.tsx
+++ src/ui/layout/AppLayout.tsx
@@ -1,13 +1,21 @@
-import { Outlet } from 'react-router-dom'
+import { useEffect } from 'react'
+import { Outlet, useLocation } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 
 import Header from './Header'
 import Main from './Main'
 import Nav from './Nav'
 import IReduxStore from '../../Models/reduxStore'
+import routes from '../../routes/routes'
 
 export default function AppLayout() {
+    const location = useLocation()
+    const activePage = routes.find((route) => route.route === location.pathname)
     const { isOverlay } = useSelector((state: IReduxStore) => state.navbar)
+
+    useEffect(() => {
+        document.title = activePage?.title || 'mr-modir'
+    }, [activePage])
     return (
         <div
             className={`grid max-h-screen min-h-screen grid-cols-1 grid-rows-[75px_1fr] gap-6 overflow-hidden px-4 lg:grid-cols-[250px_1fr] lg:pr-0 xl:grid-cols-[300px_1fr] ${
