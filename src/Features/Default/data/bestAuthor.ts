231e6b2e11cfe30e69b2b5e5ced8f615a2a17ede
sadegh <gahsemnejad.sadegh@gmail.com>
2023-12-20 00:22:07 +0330

    fix data

--- src/Features/Default/data/bestAuthor.ts
+++ src/Features/Default/data/bestAuthor.ts
@@ -63,6 +63,18 @@ export const natureBestAuthor: IBestAuthors[] = [
     {
         id: 0,
         author: {
+            avatar: 'images/avatars/002.png',
+            name: 'رضا نوری',
+            country: 'ایران',
+        },
+        conv: 85.7,
+        chart: {
+            color: colorsData.green[500],
+        },
+    },
+    {
+        id: 1,
+        author: {
             avatar: 'images/avatars/001.png',
             name: 'محمد صادق قاسم نژاد',
             country: 'ایران',
@@ -70,6 +82,30 @@ export const natureBestAuthor: IBestAuthors[] = [
         conv: 26.7,
         chart: {
             color: colorsData.red[500],
+        },
+    },
+    {
+        id: 2,
+        author: {
+            avatar: 'images/avatars/005.png',
+            name: 'مهسا امینی',
+            country: 'ایران',
+        },
+        conv: 86.2,
+        chart: {
+            color: colorsData.red[500],
+        },
+    },
+    {
+        id: 3,
+        author: {
+            avatar: 'images/avatars/004.png',
+            name: 'محمد عبداللهی',
+            country: 'ایران',
+        },
+        conv: 43.7,
+        chart: {
+            color: colorsData.green[500],
         },
     },
 ]
